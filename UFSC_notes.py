4ffaa8ab86da3eb9b252bc42ed4330d486ea0954
Guilherme Focassio dos Santos <94575270+Guilherme-K-Santos@users.noreply.github.com>
2021-11-21 22:09:44 -0300

    Add files via upload

---
+++ UFSC_notes.py
@@ -0,0 +1,50 @@
+nota = float(input())
+
+if 0 <= nota <= 0.24999:
+    print(0.0)
+elif 0.25000 <= nota <= 0.74999:
+    print(0.5)
+elif 0.75000 <= nota <= 1.24999:
+    print(1)
+elif 1.25000 <= nota <= 1.74999:
+    print(1.5)
+elif 1.75000 <= nota <= 2.24999:
+    print(2.0)
+elif 2.25000 <= nota <= 2.74999:
+    print(2.5)
+elif 2.75000 <= nota <= 3.24999:
+    print(3)
+elif 3.25000 <= nota <= 3.74999:
+    print(3.5)
+elif 3.75000 <= nota <= 4.24999:
+    print(4)
+elif 4.25000 <= nota <= 4.74999:
+    print(4.5)
+elif 4.75000 <= nota <= 5.24999:
+    print(5)
+elif 5.25000 <= nota <= 5.74999:
+    print(5.5)
+elif 5.75000 <= nota <= 6.24999:
+    print(6)
+elif 6.25000 <= nota <= 6.74999:
+    print(6.5)
+elif 6.75000 <= nota <= 7.24999:
+    print(7)
+elif 7.25000 <= nota <= 7.74999:
+    print(7.5)
+elif 7.75000 <= nota <= 8.24999:
+    print(8.0)
+elif 8.25000 <= nota <= 8.74999:
+    print(8.5)
+elif 8.75000 <= nota <= 9.24999:
+    print(9)
+elif 9.25000 <= nota <= 9.74999:
+    print(9.5)
+elif 9.75 <= nota:
+    print(10)
+
+
+
+
+
+
